b17fbe971c4277478e7de9480d44a42ee1a94297
nester1van <nester1van@yandex.ru>
2020-03-04 17:36:29 +0400

    add RandomPictDog

---
+++ precache-manifest.5b4c5be71d6f8ee6ddd070e820170f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78c0742367356e3abcebe2bfc977ef3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3813b1181cea1c1f79c",
+    "url": "/static/css/main.4cc7a5e5.chunk.css"
+  },
+  {
+    "revision": "27ac9cb700194a54359b",
+    "url": "/static/js/2.82dcf2b9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.82dcf2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3813b1181cea1c1f79c",
+    "url": "/static/js/main.92d1183f.chunk.js"
+  },
+  {
+    "revision": "a81fa6f7deddeda149e1",
+    "url": "/static/js/runtime-main.5cf4c8c0.js"
+  },
+  {
+    "revision": "c2962b63717d40f7f4de3d57819e1f8a",
+    "url": "/static/media/defaultpict.c2962b63.png"
+  }
+]);
