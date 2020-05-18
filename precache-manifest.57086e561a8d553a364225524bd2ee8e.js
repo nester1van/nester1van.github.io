95ebabe15780e54228531f0141b6bb014409f3d2
nester1van <nester1van@yandex.ru>
2020-05-18 18:06:21 +0400

    add process.env.PUBLIC_URL to path 2

---
+++ precache-manifest.57086e561a8d553a364225524bd2ee8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f93ec838f2c23fde4f045a4421efcdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae297abece1c0c58b5ba",
+    "url": "/static/css/main.b5771178.chunk.css"
+  },
+  {
+    "revision": "5630e7069d4149da17f9",
+    "url": "/static/js/2.5b466457.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.5b466457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae297abece1c0c58b5ba",
+    "url": "/static/js/main.a44bc9d7.chunk.js"
+  },
+  {
+    "revision": "a81fa6f7deddeda149e1",
+    "url": "/static/js/runtime-main.5cf4c8c0.js"
+  },
+  {
+    "revision": "c2962b63717d40f7f4de3d57819e1f8a",
+    "url": "/static/media/defaultpict.c2962b63.png"
+  }
+]);
